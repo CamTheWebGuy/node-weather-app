902aa4b0c70fc3bf03aaff9356e253489a300130
Cameron <cameronanchondo@yahoo.com>
2020-01-29 19:53:53 -0800

    Setup App for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,9 @@
 // CLIENT SIDE JS
-fetch('http://puzzle.mead.io/puzzle').then(response => {
-  response.json().then(data => {
-    console.log(data);
-  });
-});
+// fetch('http://puzzle.mead.io/puzzle').then(response => {
+//   response.json().then(data => {
+//     console.log(data);
+//   });
+// });
 
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
@@ -20,7 +20,7 @@ weatherForm.addEventListener('submit', e => {
   messageOne.textContent = 'Loading...';
   messageTwo.textContent = '';
 
-  fetch(`http://localhost:3000/weather?address=${location}`).then(response => {
+  fetch(`/weather?address=${location}`).then(response => {
     response.json().then(data => {
       if (data.error) {
         messageOne.textContent = data.error;
